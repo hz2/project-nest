8efa58ff51dc3226adedcee396954fe3d632d3db
huc <ht@live.se>
2022-01-17 10:49:34 +0800

    todo

--- src/city-info/city-info.controller.ts
+++ src/city-info/city-info.controller.ts
@@ -1,21 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus , HttpCode } from '@nestjs/common';
 import { CityInfoService } from './city-info.service';
 import { CreateCityInfoDto } from './dto/create-city-info.dto';
 import { UpdateCityInfoDto } from './dto/update-city-info.dto';
+import { Response } from 'express';
 
 @Controller('city-info')
 export class CityInfoController {
   constructor(private readonly cityInfoService: CityInfoService) {}
 
   @Post('/create')
-  create(@Body() createCityInfoDto: CreateCityInfoDto) {
-    return this.cityInfoService.create(createCityInfoDto);
+  create(@Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
   }
+  // create(@Body() createCityInfoDto: CreateCityInfoDto) {
+  //   return this.cityInfoService.create(createCityInfoDto);
+  // }
 
   @Get()
-  findAll() {
-    return this.cityInfoService.findAll();
+  @HttpCode(502)
+  findAll(@Res({ passthrough: true }) res: Response) {
+    return 'qq'
+    // res.status(HttpStatus.OK);
+    //  res.status(HttpStatus.OK).json(['11']);
   }
+  // findAll() {
+  //   return this.cityInfoService.findAll();
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
