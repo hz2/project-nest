53e071e166168861e2f22e15c966b8cdff0e5fbd
h2 <ht@live.se>
2022-02-09 22:56:45 +0800

    update

--- src/todos/dto/new-todo.input.ts
+++ src/todos/dto/new-todo.input.ts
@@ -1,8 +1,11 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsString, Length } from 'class-validator';
 
 @InputType()
 export class NewTodoInput {
   @Field()
+  @IsString() // 文字列に制限する
+  @Length(5, 140, { message: '5文字以上140文字以内です' }) // 最低5文字、最長140文字
   name: string;
 
   @Field(() => Int)
