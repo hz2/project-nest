c351d4747cc4d8dd09e9f078a3ef6b0739d47bfd
h2 <ht@live.se>
2022-02-12 17:30:38 +0800

    fix build error

--- deployed.sh
+++ deployed.sh
@@ -1,4 +1,9 @@
 
+
+
+# enable buildx
+docker run --privileged --rm tonistiigi/binfmt --install all
+# login and build
 docker login -u lzz registry.gitlab.com < ~/.ssh/docker-gitlab  --password-stdin
 docker buildx build --platform linux/arm64,linux/amd64 -t registry.gitlab.com/hzsq/project-nest . --build-arg build_mode=prod --push
 # docker push registry.gitlab.com/hzsq/project-nest
