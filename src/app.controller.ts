621240bca1e10f94891a6c0712469ec1180480a3
h2 <ht@live.se>
2022-02-20 23:00:32 +0800

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -19,4 +19,11 @@ export class AppController {
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
+
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
 }
