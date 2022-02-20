c5f0951168966b4621c838dafd93a04d9c8def9a
h2 <ht@live.se>
2022-02-20 22:42:38 +0800

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
