0902cd1ae62393681f591493982706bc31bdfa0b
h2 <ht@live.se>
2022-02-24 23:13:10 +0800

    update

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { CountryService } from './country.service';
+import { LabService } from './country.service';
 import { CountryResolver } from './country.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Country } from './entities/country';
 import { Xzqh } from './entities/xzqh';
+import { Gua } from './entities/gua';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Country,Xzqh])],
-  providers: [CountryService, CountryResolver],
-  exports: [CountryService],
+  imports: [TypeOrmModule.forFeature([Country, Xzqh, Gua])],
+  providers: [LabService, CountryResolver],
+  exports: [LabService],
 })
-export class CountryModule {}
+export class CountryModule { }
---
+++ src/country/entities/gua.ts
@@ -0,0 +1,55 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'gua' })
+@ObjectType()
+export class Gua {
+  @PrimaryGeneratedColumn()
+  @Field()
+  pkey: number;
+
+  @Column()
+  @Field()
+  key: string;
+
+  @Column()
+  @Field()
+  qian: string;
+
+  @Column()
+  @Field()
+  dui: string;
+
+  @Column()
+  @Field()
+  li: string;
+
+  @Column()
+  @Field()
+  zhen: string;
+
+  @Column()
+  @Field()
+  xun: string;
+
+  @Column()
+  @Field()
+  kan: string;
+
+  @Column()
+  @Field()
+  gen: string;
+
+  @Column()
+  @Field()
+  kun: string;
+
+  @Column()
+  @Field()
+  remark: string;
+
+}
